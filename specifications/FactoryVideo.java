58fc33018271bb08f19f5cce87cb0d41384e1f98
Dany Chea <dany.chea@he-arc.ch>
2017-05-19 11:19:49 +0200

    specs Video

---
+++ specifications/FactoryVideo.java
@@ -0,0 +1,46 @@
+
+package ch.hearc.projet.chatvideo.specifications;
+
+public class FactoryVideo implements JPanelVideo_I
+	{
+
+	JPanelVideo_I create()
+		{
+		return null;
+		}
+
+	@Override
+	public void setImagePartenaire()
+		{
+		// TODO Auto-generated method stub
+
+		}
+
+	@Override
+	public void setVideoContrast()
+		{
+		// TODO Auto-generated method stub
+
+		}
+
+	@Override
+	public void setVideoMirror()
+		{
+		// TODO Auto-generated method stub
+
+		}
+
+	@Override
+	public void setVideoColor()
+		{
+		// TODO Auto-generated method stub
+
+		}
+
+	@Override
+	public void setVideoTextOverImage()
+		{
+		// TODO Auto-generated method stub
+
+		}
+	}
---
+++ specifications/JPanelVideo_I.java
@@ -0,0 +1,14 @@
+
+package ch.hearc.projet.chatvideo.specifications;
+
+
+
+public interface JPanelVideo_I
+	{
+	void setImagePartenaire();
+	void setVideoContrast();
+	void setVideoMirror();
+	void setVideoColor();
+	void setVideoTextOverImage();
+	}
+
